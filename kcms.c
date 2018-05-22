501f58432494fc868fca0d3a279f2199fc1c6cf2
Markus Hennecke <markus-hennecke@markus-hennecke.de>
2018-05-22 18:20:50 +0200

    Add code to test new template implementation

--- kcms.c
+++ kcms.c
@@ -1,10 +1,15 @@
 
+#include <sys/mman.h>
+#include <sys/stat.h>
 #include <err.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
+#include "buffer.h"
 #include "filehelper.h"
-#include "sitemap.h"
+#include "template.h"
 
 #ifndef CMS_CONTENT_DIR
 #error "Need CMS_CONTENT_DIR defined to compile"
@@ -13,24 +18,69 @@
 #error "Need CMS_HOSTNAME defined to compile"
 #endif
 
+static __dead void usage(void);
+
+__dead void
+usage(void)
+{
+	extern char *__progname;
+
+	fprintf(stderr, "usage: %s -f file\n", __progname);
+	exit(1);
+}
+
 
 int
 main(int argc, char **argv)
 {
-	struct sitemap *sitemap = sitemap_new(CMS_CONTENT_DIR, CMS_HOSTNAME);
-#if 0
-	printf("%s\n", sitemap_toxml(sitemap));
-#else
-	uint32_t mtime = sitemap_newest(sitemap, NULL);
-	size_t size;
-	char *out = sitemap_toxmlgz(sitemap, &size, "sitemap.xml", mtime);
+	int ch, fd = -1;
 
-	char *n = out;
-	char *last = out + size;
-	for (; n < last ; n++)
-		printf("%c", *n);
+	while ((ch = getopt(argc, argv, "f:h")) != -1) {
+		switch (ch) {
+		case 'f':
+			if ((fd = open(optarg, O_RDONLY, 0)) == -1)
+				err(1, "%s", optarg);
+			break;
+		default:
+			usage();
+		}
+	}
+	argc -= optind;
+	argv += optind;
 
-	free(out);
-#endif
+	struct tmpl_data *data = tmpl_data_new();
+
+	if (-1 == fd)
+		usage();
+
+	struct stat sb;
+	if (-1 == fstat(fd, &sb))
+		err(1, NULL);
+	void *tmpl = mmap(NULL, sb.st_size, PROT_READ, MAP_SHARED, fd, 0);
+	if (NULL == tmpl)
+		err(1, NULL);
+
+	tmpl_data_set_variable(data, "LANGUAGE", "de");
+	tmpl_data_set_variable(data, "TITLE", "Template-Test");
+	struct tmpl_loop *language_links = tmpl_data_add_loop(
+			data, "LANGUAGE_LINKS"
+		);
+
+	struct tmpl_data *d = tmpl_data_new();
+	tmpl_data_set_variable(d, "LANGUAGE_LINK",
+			"<a href=\"de-page.html\">de</a>");
+	tmpl_loop_add_data(language_links, d);
+	d = tmpl_data_new();
+	tmpl_data_set_variable(d, "LANGUAGE_LINK",
+			"<a href=\"en-page.html\">en</a>");
+	tmpl_loop_add_data(language_links, d);
+
+	struct buffer_list *out = tmpl_parse((char *)tmpl, sb.st_size, data);
+	tmpl_data_free(data);
+	close(fd);
+
+	char *result = buffer_list_concat_string(out);
+	printf("%s", result);
+
 	return 0;
 }
